777a002dd8a4c919fc02bc7f7873e0637eeb8cc0
Wagner Rodakevicz de Morais <wagnerrdemorais@gmail.com>
2022-02-19 19:53:35 -0300

    Adding swagger dependency

--- src/main/java/com/wagnerrdemorais/poll/PollApplication.java
+++ src/main/java/com/wagnerrdemorais/poll/PollApplication.java
@@ -1,9 +1,13 @@
 package com.wagnerrdemorais.poll;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Poll API", version = "1.0", description = "Simple Poll API"))
 public class PollApplication {
 
     public static void main(String[] args) {
